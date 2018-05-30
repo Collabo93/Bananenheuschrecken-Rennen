e135a5be20b304fb235f01eba669138934cdd254
Collabo93 <38863391+Collabo93@users.noreply.github.com>
2018-05-30 17:40:03 +0200

    Smooth scrolling

--- js/functions.js
+++ js/functions.js
@@ -3,6 +3,7 @@ var time = new Date().getTime()/1000;
 var load = document.getElementById("load_everything");
 load.style.display = 'none';
 
+//Loading Screen
 window.addEventListener('load', function(){
 	var elapsed = new Date().getTime() / 1000 - time;
 	while(elapsed <= 1 ){
@@ -11,3 +12,41 @@ window.addEventListener('load', function(){
 	load.style.display = 'initial';
 	overlay.style.display = 'none';
 })
+
+
+//JQuery smooth scrolling
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
